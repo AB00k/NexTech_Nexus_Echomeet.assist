ff6e0036d57bcdb77d7063634ddb0e21b938ba6c
Abdul Basit <abdulbasitab00k@gmail.com>
2023-08-19 13:29:39 +0500

    Update LLM.py
    Removed API's

--- LLM.py
+++ LLM.py
@@ -3,6 +3,6 @@ from langchain.llms import OpenAI
 from faster_whisper import WhisperModel
 from elevenlabs import set_api_key
 
-llm = OpenAI(openai_api_key='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')
+llm = OpenAI(openai_api_key=OPENAI_API_KEY)
 audio_transcribe_model = WhisperModel("medium")
-set_api_key("2ef0d67d9c42500d65ecbb877bdc4660")
+set_api_key(ELVENLAB_API_KEY)
