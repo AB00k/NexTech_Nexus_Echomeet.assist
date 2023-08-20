71970a6607785b68fb36316556eb3bcc505525f7
Asadullah <140200762+Ich-Asadullah@users.noreply.github.com>
2023-08-21 01:23:33 +0500

    Update Agent_implementation.py

--- streamlit_app/codes/Agent_implementation.py
+++ streamlit_app/codes/Agent_implementation.py
@@ -3,18 +3,18 @@ from langchain.agents.agent_toolkits import ZapierToolkit
 from langchain.utilities.zapier import ZapierNLAWrapper
 from langchain.agents import initialize_agent
 from langchain.agents import AgentType
-import os
-from dotenv import load_dotenv
+from Keys import *
+# from dotenv import load_dotenv
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '..', '..', '.env')
-load_dotenv(dotenv_path=dotenv_path)
+# dotenv_path = os.path.join(os.path.dirname(os.getcwd()),'.env.sample')
+# load_dotenv(dotenv_path=dotenv_path)
 
-# Fetch the API key from environment variables
-zapier_nla_api_key = os.getenv("ZAPIER_NLA_API_KEY")
+# # Fetch the API key from environment variables
+# zapier_nla_api_key = "sk-ak-IOi22QQhkYLEphxT6ue8ASelM9"
 
 
 
-zapier = ZapierNLAWrapper(zapier_nla_api_key=zapier_nla_api_key)
+zapier = ZapierNLAWrapper(zapier_nla_api_key=ZAPIER_API_KEY)
 toolkit = ZapierToolkit.from_zapier_nla_wrapper(zapier)
 
 
